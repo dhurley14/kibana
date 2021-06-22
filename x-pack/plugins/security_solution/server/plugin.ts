52eab943b93a3190550ccd6e21dac01a72032f8a
Devin Hurley <devin.hurley@elastic.co>
2021-06-22 10:26:05 -0400

    squashed commit
    WIP - trying to fix integration tests, broken authz for observer user / role
    
    updates authz feature builder to what ying had before we messed it up in our branch
    
    fixes integration tests
    
    add rac api access to apm
    
    adds getIndex functionality which requires the asset name to be passed in, same style as in the rule registry data client, adds update integration tests
    
    fix small merge conflict and update shell script
    
    fix merge conflict in alerting test file
    
    fix most type errors
    
    fix the rest of the type failures
    
    fix integration tests
    
    fix integration tests
    
    fix type error with feature registration in apm
    
    fix integration tests in apm and security solution
    
    fix type checker
    
    fix jest tests for apm
    
    remove console.error statements for eslint
    
    fix type check
    
    update security solution jest tests
    
    cleaning up PR and adding basic unit tests
    
    still need to clean up types in tests and update one test file
    
    fixes snapshot for signals template
    
    fix tests
    
    fix type check failures
    
    update cypress test
    
    undo changes in alert authz class, updates alert privilege in apm feature to 'read', utilizes the 'rule' object available in executor params over querying for the rule SO directly
    
    remove verbose logging from detection api integration tests
    
    fix type
    
    fix jest tests, adds missing mocked rule object to alert executor params
    
    [RAC] [RBAC] adds function to get alerts-as-data index name (#6)
    
    * WIP - test script and route in rule registry to pull index name. I need to test out adding this route within the APM and sec sol plugins specifically and see if they spit back the same .alerts index but with the appropriate asset name despite not providing one.
    
    WIP - DO NOT DELETE THIS CODE
    
    minor cleanup
    
    updates client to require passing in index name, which is now available through the alerts as data client function getAlertsIndex
    
    fix types
    
    * remove outdated comment
    
    update README, adds integration test (skipped) for testing authz with search strategy (#8)
    
    * WIP
    
    * update README, adds integration test (skipped) for testing authz with search strategy
    
    * fix rebase issues
    
    * adds typedoc docs
    
    * adds SKIPPED integration test for timeline search strategy to be unskipped once authorization is added to search strategy
    
    * removes unused references to the rule data client within the rule registry
    
    squashed commit (#11)
    
    * clean up commented out code, update PR per initial comments
    
    * introduce index param to get route again, allowing user to specify index to search
    
    * updating feature privileges UI to allow user to have all, read, none on alerts
    
    Co-authored-by: Yara Tercero <yara.tercero@elastic.co>
    
    update tests
    
    WIP - updated shell scripts
    
    fixes scripts
    
    fix update route indexName -> index
    
    Merge pull request #12 from yctercero/rbac_update_tests
    
    Updates tests that were previously failing and addresses some feedback.

--- x-pack/plugins/security_solution/server/plugin.ts
+++ x-pack/plugins/security_solution/server/plugin.ts
@@ -358,7 +358,7 @@ export class Plugin implements IPlugin<PluginSetup, PluginStart, SetupPlugins, S
         all: {
           app: [...securitySubPlugins, 'kibana'],
           catalogue: ['securitySolution'],
-          api: ['securitySolution', 'lists-all', 'lists-read'],
+          api: ['securitySolution', 'lists-all', 'lists-read', 'rac'],
           savedObject: {
             all: ['alert', 'exception-list', 'exception-list-agnostic', ...savedObjectTypes],
             read: [],
@@ -379,7 +379,7 @@ export class Plugin implements IPlugin<PluginSetup, PluginStart, SetupPlugins, S
         read: {
           app: [...securitySubPlugins, 'kibana'],
           catalogue: ['securitySolution'],
-          api: ['securitySolution', 'lists-read'],
+          api: ['securitySolution', 'lists-read', 'rac'],
           savedObject: {
             all: [],
             read: ['exception-list', 'exception-list-agnostic', ...savedObjectTypes],
